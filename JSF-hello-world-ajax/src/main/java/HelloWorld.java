86bd84e1a9ee2cee1ad269808933a6d4d71e4e58
sminne <minne.sebastien@gmail.com>
2016-04-04 20:12:30 +0200

    ajax calll to toggle a boolean and display or not the greetings

--- JSF-hello-world-ajax/src/main/java/HelloWorld.java
+++ JSF-hello-world-ajax/src/main/java/HelloWorld.java
@@ -1,28 +1,43 @@
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 @ManagedBean
+@SessionScoped
 public class HelloWorld {
 
-    private String firstName = "John";
-    private String lastName = "Doe";
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
+    private boolean display = Boolean.TRUE;
+    private boolean displaypart = Boolean.TRUE;
+    
+    
     public String showGreeting() {
-        return "Hello" + " " + firstName + " " + lastName + "!";
+        return "Hello world !!";
     }
+    
+    public boolean shouldDisplay(){
+    	System.out.println("Should display ? "+this.display);
+    	return this.display ;
+    }
+    
+    public void toggleDisplay(){
+    	System.out.println("[before]toggle display ... "+this.display);
+    	this.display = !this.display;
+    	System.out.println("[after]toggle display ... "+this.display);
+    }
+    
+    public String showPart() {
+        return "world !!";
+    }
+    
+    public boolean shouldDisplayPart(){
+    	System.out.println("Should display part ? "+this.displaypart);
+    	return this.displaypart ;
+    }
+    
+    public void toggleDisplayPart(){
+    	System.out.println("[before]toggle display part ... "+this.displaypart);
+    	this.displaypart = !this.displaypart;
+    	System.out.println("[after]toggle display part ... "+this.displaypart);
+    }
+    
+    
 }
